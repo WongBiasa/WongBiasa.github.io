2ae4904de28289a301fe51b15a26e8b57daaf722
WongBiasa <74451855+WongBiasa@users.noreply.github.com>
2021-11-28 13:58:17 +0700

    Update user.php

--- user.php
+++ user.php
@@ -1,27 +1,3 @@
-<!Doctype html>
-<html>
-	<body>
 <?php
-	function get_client_browser() {
-    $browser = '';
-    if(strpos($_SERVER['HTTP_USER_AGENT'], 'Netscape'))
-        $browser = 'Netscape';
-    else if (strpos($_SERVER['HTTP_USER_AGENT'], 'Firefox'))
-        $browser = 'Firefox';
-    else if (strpos($_SERVER['HTTP_USER_AGENT'], 'Chrome'))
-        $browser = 'Chrome';
-    else if (strpos($_SERVER['HTTP_USER_AGENT'], 'Opera'))
-        $browser = 'Opera';
-    else if (strpos($_SERVER['HTTP_USER_AGENT'], 'MSIE'))
-        $browser = 'Internet Explorer';
-    else
-        $browser = 'Other';
-    return $browser;
-}
+echo $_SERVER['HTTP_USER_AGENT'];
 ?>
-<?php
-.$_SERVER['HTTP_USER_AGENT'];
-.get_client_browser().
-?>
-<body>
-</html>
