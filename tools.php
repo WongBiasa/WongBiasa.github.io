139fe70b6cdc4b566d07bd7e81c30bc8114cd6ee
WongBiasa <74451855+WongBiasa@users.noreply.github.com>
2021-12-01 09:43:27 +0700

    Create tools.php

---
+++ tools.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+  
+<head>
+    <!--content type : HTML -->
+    <meta http-equiv="content-type" 
+        content="text/html; charset=UTF-8">
+  
+    <!-- Making viewport responsive -->
+    <meta name="viewport" content=
+        "width=device-width,minimum-scale=1,
+        initial-scale=1">
+
+        <title>Hedy Fake | Web</title>
+  
+    <!-- Loading jQuery, jQuery.terminal, 
+        and style sheet -->
+    <script src=
+        "https://code.jquery.com/jquery-3.3.1.min.js">
+    </script>
+  
+    <script src=
+"https://unpkg.com/jquery.terminal/js/jquery.terminal.min.js">
+    </script>
+    <script src="perintah.js"></script>
+  
+    <link rel="stylesheet" href=
+"https://unpkg.com/jquery.terminal/css/jquery.terminal.min.css" />
+</head> 
+  <audio id="inthedark" src="https://b.top4top.io/m_2158lzxmc0.mp3"></audio>
+  <audio id="umbrellaremix" src="https://f.top4top.io/m_2156qojxk1.mp3"></audio>
+  <audio id="dontlike" src="https://k.top4top.io/m_2157fxzi90.mp3"></audio>
+  <audio id="fallenkingdom" src="https://k.top4top.io/m_2137zt60g0.mp3"></audio>
+  <audio id="dragonknight" src="https://b.top4top.io/m_2137ht1fi3.mp3"></audio>
+  <audio id="yesterevasie" src="https://c.top4top.io/m_2158nu4v41.mp3"></audio>
+<body>
+  <script>
+      // In body tag
+$('body').terminal({
+	saya: function (name) {
+		this.echo('Hello, ' + name +
+			' Welcome to My Website');
+	},
+    
+       ls: function() {
+             this.echo('./covid.py ./ig.py ./yt.py');
+       }
+
+}, {
+	greetings: 'Microsoft Windows [Version 10.0.19042.1348]'
+			+ '\n(c) Microsoft Corporation. All rights reserved.'
+});
+
+  </script>
+  <style>
+      .terminal{
+          --color: rgba(0, 200, 0, 0.99);
+          ---animation: terminal-glow;
+          --glow: 1;
+      }
+  </style>
+</body>
+</html>
